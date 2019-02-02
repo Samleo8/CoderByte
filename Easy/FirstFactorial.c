0e46bbf8f879a8d9c2f8c5b714f9d7cff8ded232
Samleo <ec3team@gmail.com>
2019-02-02 20:07:13 +0800

    Updated both C files to have Question info

--- Easy/FirstFactorial.c
+++ Easy/FirstFactorial.c
@@ -1,3 +1,8 @@
+/*
+QUESTION:
+Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+*/
+
 #include <stdio.h>
 
 int FirstFactorial(int n) {
--- Easy/FirstReverse.c
+++ Easy/FirstReverse.c
@@ -1,3 +1,8 @@
+/*
+QUESTION:
+Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string "sredoC dna dlroW olleH".
+*/
+
 #include <stdio.h>
 #include <string.h>
 
